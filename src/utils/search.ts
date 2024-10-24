c893ce794a58b9b0db7e79ef7febb7e414a23cca
Xevzzy <xevzzy.dev@gmail.com>
2024-10-24 12:02:14 -0500

    Actualización de imágenes y modificaciones en componentes y utilidades

--- src/utils/search.ts
+++ src/utils/search.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // search.ts
 
 let searchValue: string = ''; // Variable para almacenar el valor de búsqueda
@@ -11,3 +12,18 @@ export function setSearchValue(value: string): void {
 export function getSearchValue(): string {
     return searchValue;
 }
+=======
+// // search.ts
+
+// let searchValue: string = ''; // Variable para almacenar el valor de búsqueda
+
+// // Función para establecer el valor de búsqueda
+// export function setSearchValue(value: string): void {
+//     searchValue = value;
+// }
+
+// // Función para obtener el valor de búsqueda actual
+// export function getSearchValue(): string {
+//     return searchValue;
+// }
+>>>>>>> 1ecfeeb (Remaster)
